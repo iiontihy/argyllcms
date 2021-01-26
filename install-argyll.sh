58aac193b360df50a23d3c675e7b4c7b4337e294
iion.tihy@gmail.com <iion.tihy@gmail.com>
2021-01-26 18:15:39 +0200

    Update of install-argyll.sh helper

--- install-argyll.sh
+++ install-argyll.sh
@@ -1,14 +1,12 @@
-mkdir ./out
-echo "Argyll compile start" > ./compile-started.mark
+#!/bin/bash
 
-jam -f ./Jambase
+./ compile-argyll.sh
 
-# uncomment below for automated install of the binaries to /usr/bin
-# sudo cp -vf `find ./ -type f -newer ./compile-started.mark |\
-# grep -v "./out" |\
-# grep -v "./.git" |\
-# grep -v "\.o" |\
-# grep -v "\.a" |\
-# grep -v "\.h" |\
-# grep -v "\.c"`\
-# /usr/bin
+sudo cp -vf `find ./ -type f -newer ./compile-started.mark |\
+grep -v "./out" |\
+grep -v "./.git" |\
+grep -v "\.o" |\
+grep -v "\.a" |\
+grep -v "\.h" |\
+grep -v "\.c"`\
+/usr/bin
