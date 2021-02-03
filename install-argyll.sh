a05042fdd9cd4863b61e6ebcdf34c384a056bfa5
dimitarmd <dimitar.m.dimitrov@gmail.com>
2021-02-03 13:06:21 +0200

    Corrections of the install script

--- install-argyll.sh
+++ install-argyll.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-./ compile-argyll.sh
+#./ compile-argyll.sh
 
-sudo cp -vf `find ./ -type f -newer ./compile-started.mark |\
-grep -v "./out" |\
-grep -v "./.git" |\
-grep -v "\.o" |\
-grep -v "\.a" |\
-grep -v "\.h" |\
-grep -v "\.c"`\
-./out
+cp -vf `find ./ -type f -newer ./compile-started.mark |\
+ grep -v "./out" |\
+ grep -v "./.git" |\
+ grep -v "\.o" |\
+ grep -v "\.a" |\
+ grep -v "\.h" |\
+ grep -v "\.c"`\
+ ./out
